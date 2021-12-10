4f189d931630082d10ab9234770631b82373e1f2
Newman <newmer8@ad.vzwcorp.com>
2021-12-10 01:37:22 -0600

    add movie modal diagram--create app server& return routes movie/crew/cast

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const path = require(path)
+
+//const {Movie, Cast, Crew} = require('./index')
+const {Movie} = require('./models/movie')
+const {Cast} = require('./models/cast')
+const {Crew} = require('./models/crew')
+
+const app = express()
+const port = 3000
+
+//express to read json request bodies
+app.use(express.json());
+
+//return all movies
+app.get('/movies', async (req,res) => {
+    console.log(req)
+    const allMovies = await Movie.findAll()
+    res.json(allMovies)
+})
+//return all cast
+app.get('/cast', async (req,res) => {
+    console.log(req)
+    const allCasts = await Cast.findAll()
+    res.json(allCasts)
+})
+
+//return all crew
+app.get('/crew', async (req,res) => {
+    console.log(req)
+    const allCrews = await Crew.findAll()
+    res.json(allCrews)
+})
+
+
+
+app.listen(port, () => {
+    console.log('Server listening at http://localhost:${port}')
+})
