503a2f47595b32e23f342fb508cf9b61a32dcafe
Newman <newmer8@ad.vzwcorp.com>
2021-12-10 19:11:57 -0600

    update app.js CRUD

--- app.js
+++ app.js
@@ -39,12 +39,41 @@ app.post('/movies', async (req,res) =>{
     res.send("Movie Created")
 })
 
+//create new cast
+app.post('/casts', async (req,res) =>{
+    let newCast = await Cast.create(req.body)
+    res.send("Cast Created")
+})
+
+//create new crew
+app.post('/crews', async (req,res) =>{
+    let newCrew = await Crew.create(req.body)
+    res.send("Crew Created")
+})
+
+
 //update one movie by id
 app.put('/movies/:id', async (req,res) => {
     let updatedMovie = await Movie.update(req.body, {
         where : {id:req.params.id}
     })
     res.send(updatedMovie ? "Movie Updated" : "Update Failed")
+})
+
+//update one cast by id
+app.put('/casts/:id', async (req,res) => {
+    let updatedCast = await Cast.update(req.body, {
+        where : {id:req.params.id}
+    })
+    res.send(updatedCast ? "Cast Updated" : "Update Failed")
+})
+
+//update one crew by id
+app.put('/crews/:id', async (req,res) => {
+    let updatedCrew = await Crew.update(req.body, {
+        where : {id:req.params.id}
+    })
+    res.send(updatedCrew ? "Crew Updated" : "Update Failed")
 })
 
 //delete one movie by id
@@ -56,7 +85,23 @@ app.delete('/movies/:id', async (req,res) => {
     res.send(deleted ? "Deleted Movie" : "Deletion Failed")
 })
 
+//delete one cast by id
+app.delete('/casts/:id', async (req,res) => {
+    const deleted = await Cast.destroy({
+        where: {id: req.params.id}
+    })
+    //use boolen return value from destroy method return to generate a string message
+    res.send(deleted ? "Deleted Cast" : "Deletion Failed")
+})
 
+//delete one crew by id
+app.delete('/crews/:id', async (req,res) => {
+    const deleted = await Crew.destroy({
+        where: {id: req.params.id}
+    })
+    //use boolen return value from destroy method return to generate a string message
+    res.send(deleted ? "Deleted Crew" : "Deletion Failed")
+})
 
 
 
